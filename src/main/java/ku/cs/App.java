1209936e39f5629e2ceec661c27027ff05d20b3a
isFrince <sittanon.m@ku.th>
2021-08-26 02:41:41 +0700

    register update 002 & txt -> csv

--- src/main/java/ku/cs/App.java
+++ src/main/java/ku/cs/App.java
@@ -27,7 +27,11 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXRouter.bind(this, stage, "JAVAPAI",1024.0,768.0);
         configRoute();
+<<<<<<< Updated upstream
         FXRouter.goTo("home");
+=======
+        FXRouter.goTo("home");
+>>>>>>> Stashed changes
     }
 
     private static void configRoute() {
--- src/main/java/ku/cs/shop/controllers/RegisterController.java
+++ src/main/java/ku/cs/shop/controllers/RegisterController.java
@@ -2,7 +2,10 @@ package ku.cs.shop.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
 import ku.cs.shop.models.User ;
 
 import java.io.File;
@@ -11,39 +14,66 @@ import java.io.IOException;
 
 public class RegisterController {
 
-    private User user ;
-    // controller เชื่อมต่อกับ view เพื่อรับข้อมูลมาเก็บ
+    // controller เชื่อมต่อกับ view เพื่อรับข้อมูล
     @FXML private TextField firstNameTextField ;
     @FXML private TextField lastNameTextField ;
     @FXML private TextField userNameTextField ;
-    @FXML private TextField passwordTextField ;
-    @FXML private TextField passwordRecheckTextField ;
+    @FXML private PasswordField passwordField ;
+    @FXML private PasswordField checkPasswordField ;
     @FXML private TextField birthDayTextField ;
     @FXML private TextField birthMonthTextField ;
     @FXML private TextField birthYearTextField ;
+    @FXML private Label passwordCompareLabel ;
+    @FXML private Label registerErrorLabel ;
 
+    //private boolean usernameCheck = true ;
+    private boolean passwordCheck = false ;
 
-//    public void checkAndComparePassword(ActionEvent actionEvent) {
-//        String passwordStr = passwordTextField.getText() ;
-//        String passwordRecheckStr = passwordRecheckTextField.getText() ;
-//        //ตรวจสอบความปลอดภัยของรหัสผ่านตามที่กำหนด
-//
-//        //ตรวจสอบการยืนยันรหัสผ่าน
-//        if (!(passwordStr.equals(passwordRecheckStr))){
-//            System.err.println("รหัสผ่านไม่ตรงกัน");
-//            System.err.println("ตรวจสอบรหัสผ่านอีกครั้ง");
-//        }
-//    }
-//
-//    public void checkUserNameCondition(ActionEvent actionEvent) {
+    //ตรวจสอบการยืนยันรหัสผ่าน
+    @FXML
+    public void comparePassword() {
+        String passwordStr = passwordField.getText();
+        String passwordRecheckStr = checkPasswordField.getText();
+
+        if (passwordStr.equals(passwordRecheckStr)) {
+            passwordCompareLabel.setText("");
+            passwordCheck = true ;
+        }
+        else {
+            passwordCompareLabel.setText("รหัสผ่านไม่ตรงกัน โปรดตรวจสอบรหัสผ่าน");
+            passwordCheck = false ;
+        }
+
+    }
+
+    //ตรวจสอบรหัสผ่านตามเงื่อนไข  //setTextFill(Color.rgb(21, 117, 84));
+
+//    public void checkUserNameCondition() {
 //        String userNameStr = userNameTextField.getText() ;
 //        //ตรวจสอบ username ว่าตรงเงื่อนไขมั้ย
 //
 //        //ตรวจสอบ username ว่าซ้ำมั้ย
 //    }
-    //ปุ่ม Login
+
+    //ปุ่ม register
     @FXML
     public void registerButton(ActionEvent actionEvent) {
+        //ตรวจสอบความถูกต้อง username password
+        if(!(passwordCheck/*&&usernameCheck*/)) {
+            registerErrorLabel.setText("พบข้อผิดพลาด กรุณาตรวจสอบข้อมูล");
+            registerErrorLabel.setTextFill(Color.rgb(210, 39, 30));
+            return;
+        }
+        // ตรวจสอบว่าทุกช่องมีข้อมูล
+        else if (firstNameTextField.getText().equals("")||lastNameTextField.getText().equals("")||
+        userNameTextField.getText().equals("")||checkPasswordField.getText().equals("")||
+        birthDayTextField.getText().equals("")||birthMonthTextField.getText().equals("")||birthYearTextField.getText().equals("")) {
+            registerErrorLabel.setText("ข้อมูลไม่ครบถ้วน กรุณาตรวจสอบข้อมูล");
+            registerErrorLabel.setTextFill(Color.rgb(210, 39, 30));
+            return;
+        }
+
+        registerErrorLabel.setTextFill(Color.rgb(255, 255, 255));
         writeUserInfo();
         try {
             com.github.saacsos.FXRouter.goTo("login");
@@ -57,31 +87,34 @@ public class RegisterController {
     //เก็บข้อมูลของ user
     @FXML
     public void writeUserInfo() {
+
         String firstNameStr = firstNameTextField.getText() ;
         String lastNameStr = lastNameTextField.getText() ;
         String userNameStr = userNameTextField.getText() ;
-        String passwordStr = passwordTextField.getText() ;
+        String passwordStr = passwordField.getText() ;
         String birthDayStr = birthDayTextField.getText() ;
         String birthMonthStr = birthMonthTextField.getText() ;
         String birthYearStr = birthYearTextField.getText() ;
 
         //นำข้อมูล String เก็บใน FieldClass และบันทึกลง CSV
-        File userData = new File("src/main/java/ku/cs/shop/userData.txt");
+        File userData = new File("src/main/java/ku/cs/shop/userData.csv");
 
         FileWriter writer ;
         try {
             writer = new FileWriter(userData, true);
-            writer.write(firstNameStr+","+lastNameStr+","+userNameStr+","+passwordStr+","+
+            writer.write(""+userNameStr+","+firstNameStr+","+lastNameStr+","+passwordStr+","+
                     birthDayStr+","+birthMonthStr+","+birthYearStr+"\r\n");
             writer.close();
         }
         catch (IOException e){
-            System.err.println("เกิดข้อผิดพลาด สมัครไม่สำเร็จ");
+            e.printStackTrace();
+            System.err.println("สมัครไม่สำเร็จ");
         }
-        //ยังไม่ได้ใช้ user = new User(firstNameStr, lastNameStr, userNameStr, passwordStr, birthDayStr, birthMonthStr, birthYearStr);
     }
+
+    //ปุ่มกลับไปหน้า login
     @FXML
-    public void handleToLoginButton(ActionEvent actionEvent) { //ปุ่มสำหรับกดไปหน้า หนังสือทั้งหมด (เพจหลัก)
+    public void handleToLoginButton(ActionEvent actionEvent) {
         try {
             com.github.saacsos.FXRouter.goTo("login");
         } catch (IOException e) {
