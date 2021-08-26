f8ce3a1871e8d97d81321ea3997108aa15d7a687
isFrince <sittanon.m@ku.th>
2021-08-26 20:48:40 +0700

    register update 003 ver.Kea yupp yupp

--- src/main/java/ku/cs/shop/controllers/RegisterController.java
+++ src/main/java/ku/cs/shop/controllers/RegisterController.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 public class RegisterController {
 
     // controller เชื่อมต่อกับ view เพื่อรับข้อมูล
+    User user = new User();
     @FXML private TextField firstNameTextField ;
     @FXML private TextField lastNameTextField ;
     @FXML private TextField userNameTextField ;
@@ -25,68 +26,27 @@ public class RegisterController {
     @FXML private TextField birthYearTextField ;
     @FXML private Label passwordCompareLabel ;
     @FXML private Label registerErrorLabel ;
+    @FXML private Label passwordConditionCheckLabel ;
 
-    //private boolean usernameCheck = true ;
-    private boolean passwordCheck = false ;
-
-    //ตรวจสอบการยืนยันรหัสผ่าน
-    @FXML
-    public void comparePassword() {
-        String passwordStr = passwordField.getText();
-        String passwordRecheckStr = checkPasswordField.getText();
-
-        if (passwordStr.equals(passwordRecheckStr)) {
-            passwordCompareLabel.setText("");
-            passwordCheck = true ;
+    @FXML //ทำงานเมื่อกรอกรหัส
+    public void keyPassword() {
+        if (user.checkPasswordCondition(passwordField.getText())){
+            passwordConditionCheckLabel.setText("รหัสผ่านนี้สามารถใช้ได้") ;
+            passwordConditionCheckLabel.setTextFill(Color.rgb(21, 117, 84));
         }
         else {
-            passwordCompareLabel.setText("รหัสผ่านไม่ตรงกัน โปรดตรวจสอบรหัสผ่าน");
-            passwordCheck = false ;
+            passwordConditionCheckLabel.setText("รหัสผ่านไม่ตรงตามรูปแบบที่กำหนด");
+            passwordConditionCheckLabel.setTextFill(Color.rgb(210, 39, 30));
         }
-
     }
-
-    //ตรวจสอบรหัสผ่านตามเงื่อนไข  //setTextFill(Color.rgb(21, 117, 84));
-
-//    public void checkUserNameCondition() {
-//        String userNameStr = userNameTextField.getText() ;
-//        //ตรวจสอบ username ว่าตรงเงื่อนไขมั้ย
-//
-//        //ตรวจสอบ username ว่าซ้ำมั้ย
-//    }
+    @FXML //ทำงานเมื่อกรอกยืนยันรหัส
+    public void keyCheckPassword() {
+        passwordCompareLabel.setText(user.comparePassword(passwordField.getText(), checkPasswordField.getText()));
+    }
 
     //ปุ่ม register
     @FXML
     public void registerButton(ActionEvent actionEvent) {
-        //ตรวจสอบความถูกต้อง username password
-        if(!(passwordCheck/*&&usernameCheck*/)) {
-            registerErrorLabel.setText("พบข้อผิดพลาด กรุณาตรวจสอบข้อมูล");
-            registerErrorLabel.setTextFill(Color.rgb(210, 39, 30));
-            return;
-        }
-        // ตรวจสอบว่าทุกช่องมีข้อมูล
-        else if (firstNameTextField.getText().equals("")||lastNameTextField.getText().equals("")||
-        userNameTextField.getText().equals("")||checkPasswordField.getText().equals("")||
-        birthDayTextField.getText().equals("")||birthMonthTextField.getText().equals("")||birthYearTextField.getText().equals("")) {
-            registerErrorLabel.setText("ข้อมูลไม่ครบถ้วน กรุณาตรวจสอบข้อมูล");
-            registerErrorLabel.setTextFill(Color.rgb(210, 39, 30));
-            return;
-        }
-
-        registerErrorLabel.setTextFill(Color.rgb(255, 255, 255));
-        writeUserInfo();
-        try {
-            com.github.saacsos.FXRouter.goTo("login");
-        }
-        catch (IOException e) {
-            System.err.println("ไปที่หน้า login ไม่ได้");
-            System.err.println("ให้ตรวจสอบการกำหนด route");
-        }
-    }
-
-    //เก็บข้อมูลของ user
-    @FXML
-    public void writeUserInfo() {
 
         String firstNameStr = firstNameTextField.getText() ;
         String lastNameStr = lastNameTextField.getText() ;
@@ -95,20 +55,27 @@ public class RegisterController {
         String birthDayStr = birthDayTextField.getText() ;
         String birthMonthStr = birthMonthTextField.getText() ;
         String birthYearStr = birthYearTextField.getText() ;
+        user.setFirstName(firstNameStr);
+        user.setLastName(lastNameStr);
+        user.setUserName(userNameStr);
+        user.setPassword(passwordStr);
+        user.setBirthDay(birthDayStr);
+        user.setBirthMonth(birthMonthStr);
+        user.setBirthYear(birthYearStr);
 
-        //นำข้อมูล String เก็บใน FieldClass และบันทึกลง CSV
-        File userData = new File("src/main/java/ku/cs/shop/userData.csv");
+        registerErrorLabel.setText(user.dataCheck());
 
-        FileWriter writer ;
-        try {
-            writer = new FileWriter(userData, true);
-            writer.write(""+userNameStr+","+firstNameStr+","+lastNameStr+","+passwordStr+","+
-                    birthDayStr+","+birthMonthStr+","+birthYearStr+"\r\n");
-            writer.close();
+        if (!(user.getDataCheck())) {
+            return;
         }
-        catch (IOException e){
-            e.printStackTrace();
-            System.err.println("สมัครไม่สำเร็จ");
+
+        user.writeUserInfo();
+
+        try {
+            com.github.saacsos.FXRouter.goTo("login");
+        } catch (IOException e) {
+            System.err.println("ไปที่หน้า login ไม่ได้");
+            System.err.println("ให้ตรวจสอบการกำหนด route");
         }
     }
 
--- src/main/java/ku/cs/shop/models/User.java
+++ src/main/java/ku/cs/shop/models/User.java
@@ -1,5 +1,10 @@
 package ku.cs.shop.models;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 public class User {
 
     private String firstName ;
@@ -9,8 +14,10 @@ public class User {
     private String birthDay ;
     private String birthMonth ;
     private String birthYear ;
+    private boolean passwordCheck = false, passwordCondition = false, dataCheck = false;
 
     //เก็บค่าเริ่มต้น
+    public User(){}
     public User(String firstName, String lastName, String userName, String password, String birthDay, String birthMonth, String birthYear){
         this.firstName = firstName ;
         this.lastName = lastName ;
@@ -21,7 +28,6 @@ public class User {
         this.birthYear = birthYear ;
     }
 
-    //สำหรับส่งค่าแสดงผล
     public String getFirstName() {
         return firstName;
     }
@@ -43,15 +49,87 @@ public class User {
     public String getBirthYear() {
         return birthYear;
     }
+    public boolean getDataCheck() {return dataCheck; }
 
-    //สำหรับแก้ไขค่า ไม่สามารถแก้ไข userName และส่วนของวันเกิด
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+    public void setUserName(String userName) { this.userName = userName; }
+    public void setBirthDay(String birthDay) { this.birthDay = birthDay; }
+    public void setBirthMonth(String birthMonth) { this.birthMonth = birthMonth; }
+    public void setBirthYear(String birthYear) { this.birthYear = birthYear; }
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    //    public void checkUserNameCondition() {
+//        String userNameStr = userNameTextField.getText() ;
+//        //ตรวจสอบ username ว่าตรงเงื่อนไขมั้ย
+//
+//        //ตรวจสอบ username ว่าซ้ำมั้ย
+//    }
+
+    //ตรวจสอบรหัสผ่านตามเงื่อนไข
+    public boolean checkPasswordCondition (String password)  {
+        if (password.length()<8 ||!(Pattern.matches("[a-zA-Z0-9]+", password))) {
+            this.passwordCondition = false ;
+            return false ;
+        }
+        else {
+            this.passwordCondition = true ;
+            return true ;
+        }
+    }
+
+    //ตรวจสอบการยืนยันรหัสผ่าน
+    public String comparePassword(String password, String passwordRecheck) {
+        if (password.equals(passwordRecheck)) {
+            this.passwordCheck = true ;
+            return "";
+        }
+        else {
+            this.passwordCheck = false ;
+            return "รหัสผ่านไม่ตรงกัน โปรดตรวจสอบรหัสผ่าน";
+        }
+
+    }
+
+    //เก็บข้อมูลของ user
+    public void writeUserInfo() {
+        //นำข้อมูล String เก็บใน FieldClass และบันทึกลง CSV
+        File userData = new File("src/main/java/ku/cs/shop/userData.csv");
+
+        FileWriter writer;
+        try {
+            writer = new FileWriter(userData, true);
+            writer.write(userName + "," + firstName + "," + lastName + "," + password + "," +
+                    birthDay + "," + birthMonth + "," + birthYear + "\r\n");
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("สมัครไม่สำเร็จ");
+        }
+    }
+
+    //เช็คการกรอกข้อมูลก่อนสมัคร
+    public String dataCheck() {
+
+        // ตรวจสอบว่าทุกช่องมีข้อมูล
+        if ((firstName.equals("")||lastName.equals("")||userName.equals("")||!(passwordCheck)||
+                birthDay.equals("")||birthMonth.equals("")||birthYear.equals(""))) {
+            this.dataCheck = false ;
+            return "ข้อมูลไม่ครบถ้วน โปรดตรวจสอบข้อมูลอีกครั้ง";
+        }
+        else if (!(this.passwordCheck && this.passwordCondition /*&&usernameCheck*/)) {
+            this.dataCheck = false ;
+            return "ข้อมูลมีข้อผิดพลาดโปรดตรวจสอบข้อมูลอีกครั้ง" ;
+        }
+        else {
+            this.dataCheck = true ;
+            return " ";
+        }
     }
 }
