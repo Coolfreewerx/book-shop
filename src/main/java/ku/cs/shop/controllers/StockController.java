0deb3a50177a0553c36911fe43ffc867f0fb3df9
Coolfreewerx <nattapon.mee@ku.th>
2021-09-03 12:14:39 +0700

    fix error

--- src/main/java/ku/cs/shop/controllers/StockController.java
+++ src/main/java/ku/cs/shop/controllers/StockController.java
@@ -24,20 +24,17 @@ public class StockController {
 
     public void setData(Book book){
         this.book = book;
-<<<<<<< HEAD
         bookNameLabel.setText(book.getBookName());
         bookTypeLabel.setText(book.getBookType());
         bookPriceLabel.setText(Double.toString(book.getBookPrice()));
         bookStockLabel.setText(Integer.toString(book.getBookStock()));
 //        bookImageView.setImage();
-=======
 //        bookNameLabel.setText(book.getBookName());
 //        bookTypeLabel.setText(book.getBookType());
 //        bookPriceLabel.setText(Double.toString(book.getBookPrice()));
 //        bookStockLabel.setText(Integer.toString(book.getBookStock()));
 //        Image image = new Image(getClass().getResourceAsStream(book.getBookImg()));
 //        bookImageView.setImage(image);
->>>>>>> cc1605d26626ecb2413671b3306d6598b9462e16
     }
 //    public void handleDecreaseStockButton(ActionEvent actionEvent) {
 //        booksList.get(1).decreaseStock(1);
