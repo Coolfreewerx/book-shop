006a6390ded61055ab81dde71f39f1944a7db995
ManeesornV <maneesorn.v@ku.th>
2021-09-08 20:01:24 +0700

    editSellermodels

---
+++ src/main/java/ku/cs/shop/controllers/ApplySellerController.java
@@ -0,0 +1,23 @@
+package ku.cs.shop.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import ku.cs.shop.models.Seller;
+
+public class ApplySellerController {
+    Seller seller = new Seller();
+    @FXML private TextField nameShopTextField;
+    @FXML private TextField addressShopTextField;
+    @FXML private TextField detailShopTextField;
+    @FXML private TextField mailShopTextField;
+    @FXML private TextField telShopTextField;
+    @FXML private TextField bankShopTextField;
+    @FXML private Button submitButton;
+
+    @FXML
+    public void initialize () {}
+
+
+}
