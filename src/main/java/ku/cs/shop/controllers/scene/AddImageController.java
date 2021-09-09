3e73200c38c17559db65993af446a4f501e317b7
isFrince <sittanon.m@ku.th>
2021-09-10 00:46:24 +0700

    image update

--- src/main/java/ku/cs/shop/controllers/scene/AddImageController.java
+++ src/main/java/ku/cs/shop/controllers/scene/AddImageController.java
@@ -1,4 +1,28 @@
 package ku.cs.shop.controllers.scene;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import ku.cs.shop.controllers.user.RegisterController;
+
 public class AddImageController {
+
+    @FXML private Button useImageButton ;
+    @FXML private Button cancelButton ;
+
+    RegisterController registerController ;
+
+    public void setRegisterController(RegisterController registerController) {
+        this.registerController = registerController;
+    }
+    
+    @FXML
+    public void handleUseButton (ActionEvent actionEvent) {
+        registerController.handleUseButton();
+    }
+    @FXML
+    public void handleCancelButton (ActionEvent actionEvent) {
+        registerController.handleCancelButton();
+    }
 }
